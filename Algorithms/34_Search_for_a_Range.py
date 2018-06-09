259c17973b3aa6b7712e73bacef167894ffd9620
Aydin <aydin-ka@yandex.ru>
2018-06-09 17:36:17 +0400

    Create 34_Search_for_a_Range.py

---
+++ Algorithms/34_Search_for_a_Range.py
@@ -0,0 +1,42 @@
+class Solution:
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        
+        call = Solution()
+        return call.searchRange(nums, target)
+    
+    def searchRange(self, nums, target):
+        result = []
+        length = len(nums)
+
+        start = 0
+        end = length
+        while start < end:
+            mid = (start + end) // 2
+            if nums[mid] == target and (mid == 0 or nums[mid - 1] != target):
+                result.append(mid)
+                break
+            if nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid
+        if not result:
+            return [-1, -1]
+
+        end = length
+        while start < end:
+            mid = (start + end) // 2
+            if nums[mid] == target and (mid == length - 1 or nums[mid + 1] != target):
+                result.append(mid)
+                break
+            if nums[mid] <= target:
+                start = mid + 1
+            else:
+                end = mid
+
+        return result
